384b076431b785e3c2a2a1ed8db5f5b86d205d12
TheIronWolfModding <vleonavicius@hotmail.com>
2019-01-11 15:49:17 -0800

    Plugin: add Morten's pattern finder.

--- Include/Utils.h
+++ Include/Utils.h
@@ -36,3 +36,46 @@ ScopeGuard<Lambda> MakeScopeGuard(Lambda const& l)
 {
   return ScopeGuard<Lambda>(l);
 };
+
+
+
+/*
+Author: Sparten (Morten Roslev)
+Website: thecrewchief.org
+*/
+
+#include <cstdint>
+#include <windows.h>
+#define LOWORD(_dw)     ((WORD)(((DWORD_PTR)(_dw)) & 0xffff))
+#define HIWORD(_dw)     ((WORD)((((DWORD_PTR)(_dw)) >> 16) & 0xffff))
+#define LODWORD(_qw)    ((DWORD)(_qw))
+#define HIDWORD(_qw)    ((DWORD)(((_qw) >> 32) & 0xffffffff))
+
+/// <summary>
+/// Searches for the first pattern in the module.
+/// </summary>
+/// <param name="module">The module to scan.</param>
+/// <param name="pattern">The pattern (Example: "\x12\xAB\x34")</param>
+/// <param name="mask">The mask (Example: "x?x")</param>
+/// <returns>The address of the found pattern or -1 if the pattern was not found.</returns>
+uintptr_t FindPattern(HMODULE module, unsigned const char* pattern, char const* mask);
+
+/// <summary>
+/// Searches for the first pattern in the memory region.
+/// </summary>
+/// <param name="start">The start address of the memory region to scan.</param>
+/// <param name="length">The length of the memory region.</param>
+/// <param name="pattern">The pattern (Example: "\x12\xAB\x34")</param>
+/// <param name="mask">The mask (Example: "x?x")</param>
+/// <returns>The address of the found pattern or -1 if the pattern was not found.</returns>
+uintptr_t FindPattern(uintptr_t start, size_t length, unsigned const char* pattern, char const* mask);
+
+/// <summary>
+/// Searches for the first pattern in the module.
+/// </summary>
+/// <param name="module">The module to scan.</param>
+/// <param name="pattern">The pattern (Example: "\x12\xAB\x34")</param>
+/// <param name="mask">The mask (Example: "x?x")</param>
+/// <param name="bytedIntoPatternToFindOffset">nr bytes into found address to add to get the pointer offset (Example: -?+?)</param>
+/// <returns>The address of the found pattern or -1 if the pattern was not found.</returns>
+uintptr_t* FindPatternForPointerInMemory(HMODULE module, unsigned const char* pattern, char const* mask, int bytedIntoPatternToFindPointer);
---
+++ Source/Utils.cpp
@@ -0,0 +1,42 @@
+#include "Utils.h"
+
+#include <psapi.h>
+
+uintptr_t FindPattern(HMODULE module, unsigned const char* pattern, char const* mask)
+{
+  MODULEINFO info = {};
+  ::GetModuleInformation(::GetCurrentProcess(), module, &info, sizeof(MODULEINFO));
+
+  return FindPattern(reinterpret_cast<uintptr_t>(module), info.SizeOfImage, pattern, mask);
+}
+
+uintptr_t* FindPatternForPointerInMemory(HMODULE module, unsigned const char* pattern, char const* mask, int bytedIntoPatternToFindOffset)
+{
+  MODULEINFO info = {};
+  ::GetModuleInformation(GetCurrentProcess(), module, &info, sizeof(MODULEINFO));
+  auto address = FindPattern(reinterpret_cast<uintptr_t>(module), info.SizeOfImage, pattern, mask);
+  if (address == -1uLL)
+    return nullptr;
+
+  address += bytedIntoPatternToFindOffset;
+  return reinterpret_cast<uintptr_t*>(address + LODWORD(*(uintptr_t*) address + 4));
+}
+
+uintptr_t FindPattern(uintptr_t start, size_t length, unsigned const char* pattern, char const* mask)
+{
+  size_t pos = 0u;
+  auto maskLength = strlen(mask) - 1;
+
+  auto startAdress = start;
+  for (auto it = startAdress; it < startAdress + length; ++it) {
+    if (*reinterpret_cast<unsigned char*>(it) == pattern[pos] || mask[pos] == '?') {
+      if (mask[pos + 1] == '\0')
+        return it - maskLength;
+
+      pos++;
+    } else
+      pos = 0;
+  }
+
+  return -1uLL;
+}
